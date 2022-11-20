c9ef3bf868af74d5695774b84ba5cfe12b72c852
Koczka Vencel <koczka990@gmail.com>
2022-11-20 17:14:29 +0100

    create database with countingdata table

---
+++ Backend/Backend.DLL/Context/DBSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DLL.Context
+{
+    public class DBSettings
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Db { get; set; }
+        public string GetConnectionString()
+        {
+            return "Host=" + Host + ":" + Port + ";Username=" + User + ";Password=" + Password + ";Database=" + Db;
+        }
+    }
+}
