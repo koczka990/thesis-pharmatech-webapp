d94db83e5db4ab1703a61ff6442d1afec650a1af
Koczka Vencel <koczka990@gmail.com>
2022-11-21 23:04:17 +0100

    create and register StatService

---
+++ Backend/Backend/Services/StatDataService.cs
@@ -0,0 +1,15 @@
+﻿using Backend.DLL.Repositories.Interfaces;
+using Backend.Services.Interfaces;
+
+namespace Backend.Services
+{
+    public class StatDataService : IStatDataService
+    {
+        private readonly IStatDataRepository statDataRepository;
+
+        public StatDataService(IStatDataRepository statDataRepository)
+        {
+            this.statDataRepository = statDataRepository;
+        }
+    }
+}
