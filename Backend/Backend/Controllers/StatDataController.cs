61c7a01f73ab9bccfb717848def6ee69b16b25ed
Koczka Vencel <koczka990@gmail.com>
2022-11-21 23:04:42 +0100

    create statDataController

---
+++ Backend/Backend/Controllers/StatDataController.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatDataController : ControllerBase
+    {
+        private readonly IStatDataService statDataService;
+        private readonly ILogger<StatDataController> logger;
+        public StatDataController(IStatDataService service, ILogger<StatDataController> logger)
+        {
+            this.statDataService = service;
+            this.logger = logger;
+        }
+    }
+}
