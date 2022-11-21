2986f9d02ea0bf326af9235ba2fc58e814038725
Koczka Vencel <koczka990@gmail.com>
2022-11-21 21:38:40 +0100

    create repository for statdata

---
+++ Backend/Backend.DLL/Repositories/Interfaces/IStatDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using Backend.DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DLL.Repositories.Interfaces
+{
+    public interface IStatDataRepository
+    {
+        public void Create(StatData countingData);
+        public void Create(List<StatData> countingData);
+        public StatData Get(int id);
+        public List<StatData> GetAll();
+        public List<StatData> GetBetween(DateTime fromTime, DateTime toTime);
+    }
+}
